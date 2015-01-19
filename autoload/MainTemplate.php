288271d8b1dc3eb40643d8be63876bf9c390fd81
datchung <innochi.contact@gmail.com>
2015-01-18 23:39:06 -0500

    1) SOC: put content files separate from code and template. 2) Refactor route functions on index.php. 3) Use current year in copyright footer. 4)

--- autoload/MainTemplate.php
+++ autoload/MainTemplate.php
@@ -1,13 +1,14 @@
 <?php
 
 class MainTemplate {
-      public static function setup($f3, $submenuItems, $submenuOffset, $content) {
+      public static function render($f3, $filePath) {
+        $pageContent = json_decode(file_get_contents('./' . $filePath, FILE_USE_INCLUDE_PATH), true);
+        $f3->set('pageContent', $pageContent);
+
         $UI_PATH = $f3->get('UI_PATH');
-
-        $f3->set('submenuItems', $submenuItems);
-
-        $f3->set('submenuOffset', $submenuOffset);
         $f3->set('submenu', $UI_PATH . 'sub-menu.html');
-        $f3->set('content', $UI_PATH . $content);
+        $f3->set('sections', $UI_PATH . 'sections.html');
+        $f3->set('year', date('Y'));
+        echo Template::instance()->render($UI_PATH . 'template.html');
       }
 }
--- index.php
+++ index.php
@@ -8,45 +8,13 @@ $f3->set('AUTOLOAD','autoload/');
 
 $f3->route('GET /',
     function($f3) {
-        $submenuItems = array(
-            array(
-                'title' => 'Skills/Experience',
-                'name' => 'skills'
-                ),
-            array(
-                'title' => 'Interests',
-                'name' => 'interests'
-                ),
-            array(
-                'title' => 'Extracurricular',
-                'name' => 'extra'
-                )
-        );
-        $submenuOffset = 'offset-by-three';
-        $content = 'about.html';
-        MainTemplate::setup($f3, $submenuItems, $submenuOffset, $content);
-
-        echo Template::instance()->render('ui/template.html');
+        MainTemplate::render($f3, 'content/about.json');
     }
 );
 
 $f3->route('GET /mobile-apps',
     function($f3) {
-        $submenuItems = array(
-            array(
-                'title' => 'Android',
-                'name' => 'android'
-                ),
-            array(
-                'title' => 'Windows Phone',
-                'name' => 'wp'
-                )
-        );
-        $submenuOffset = 'offset-by-four';
-        $content = 'mobile-apps.html';
-        MainTemplate::setup($f3, $submenuItems, $submenuOffset, $content);
-
-        echo Template::instance()->render('ui/template.html');
+        MainTemplate::render($f3, 'content/mobile-apps.json');
     }
 );
 
