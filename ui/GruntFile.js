08f0a7c2fb6929c21c2f608d24bc2ea95d5c6a3d
datchung <innochi.contact@gmail.com>
2015-01-21 21:05:04 -0500

    1) Fix csslint errors for 0px. 2) Minify and concat all css files into 1 to speed up page load

--- ui/GruntFile.js
+++ ui/GruntFile.js
@@ -1,11 +1,11 @@
 module.exports = function(grunt) {
 
-  var tasks = ['less', 'cssmin', 'csslint', 'clean'];
+  var tasks = ['less', 'csslint', 'cssmin', 'clean'];
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Project configuration.
@@ -31,20 +31,22 @@ module.exports = function(grunt) {
       }
     },
 
-    cssmin: {
-      target: {
-        files: {
-          'css/main.min.css': ['build/main.css']
-        }
-      }
-    },
-
     csslint: {
       strict: {
         options: {
           import: 2
         },
-        src: ['css/main.min.css']
+        src: ['build/main.css']
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: {
+          'css/main.min.css': ['vendor/skeleton/css/*.css', 
+          'bower_components/css-responsive-menu/dist/css-responsive-menu.min.css', 
+          'build/main.css']
+        }
       }
     },
 
